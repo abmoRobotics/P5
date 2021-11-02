7c89a3ea6a8bd7642d00ae3d53f23cd65298a9c0
etpe19 <56429616+etpe19@users.noreply.github.com>
2021-11-02 23:24:15 +0100

    Delete ControllerV1.d

--- controllers/ControllerV1/build/release/ControllerV1.d
+++
@@ -1,7 +0,0 @@
-build/release/ControllerV1.o: ControllerV1.cpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /home/marie/P5/UDP/include/UDP_Com.h \
- /home/marie/P5/UDP/include/json.h include/shrimp_protocol.h
