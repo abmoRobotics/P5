3560cb0c34aca55fb6b4e0406a817e79687e6efa
etpe19 <56429616+etpe19@users.noreply.github.com>
2021-11-02 23:15:37 +0100

    Delete ControllerV2.d

--- controllers/ControllerV2/build/release/ControllerV2.d
+++
@@ -1,6 +0,0 @@
-build/release/ControllerV2.o: ControllerV2.cpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/Motor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /home/marie/P5/UDP/include/UDP_Com.h /home/marie/P5/UDP/include/json.h
