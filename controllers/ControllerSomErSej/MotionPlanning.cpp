6b95a5576233c3a88cd0f48cc80b82040466f8ad
etpe19 <etpe19@student.aau.dk>
2021-11-08 14:31:07 +0100

    MotionPlanning

--- controllers/ControllerSomErSej/MotionPlanning.cpp
+++ controllers/ControllerSomErSej/MotionPlanning.cpp
@@ -5,21 +5,38 @@
 
 
 
-// void MotionPlanning::GetPosition(){
-    
-// }
+float* MotionPlanning::GetPosition(float t){
+    static float Position[2];
+    Position[0] = a[0][0] + a[0][1]*t + a[0][2]*(t*t) + a[0][3]*(t*t*t);
+    Position[1] = a[1][0] + a[1][1]*t + a[1][2]*(t*t) + a[1][3]*(t*t*t);
+    return Position;
+}
+
+float* MotionPlanning::GetVelocity(float t){
+    static float Velocity[2];
+    Velocity[0] = a[0][1] + 2*a[0][2]*t + 3*a[0][3]*(t*t);
+    Velocity[1] = a[1][1] + 2*a[1][2]*t + 3*a[1][3]*(t*t);
+    return Velocity;
+}
+
+float* MotionPlanning::GetAcceleration(float t){
+    static float Acceleration[2];
+    Acceleration[0] = 2*a[0][2] + 6*a[0][3]*t;
+    Acceleration[1] = 2*a[1][2] + 6*a[1][3]*t;
+    return Acceleration;
+}
 
 void MotionPlanning::InitiateTestData(){
-    DataPoints[0][0] = 0; //X0
-    DataPoints[0][1] = 0; //Y0
+    DataPoints[0][0] = 0.4; //X0
+    DataPoints[0][1] = 1.2; //Y0
     DataPoints[0][2] = 0; //T0
     
-    DataPoints[1][0] = 2; //X1
-    DataPoints[1][1] = 1; //Y1
+    DataPoints[1][0] = 0.4; //X1
+    DataPoints[1][1] = 2.0; //Y1
     DataPoints[1][2] = 1; //T1
 
-    DataPoints[2][0] = 2;
-    DataPoints[2][1] = 3;
+    DataPoints[2][0] = -0.4;
+    DataPoints[2][1] = 2.0;
     DataPoints[2][2] = 2;
 }
 
--- controllers/ControllerSomErSej/include/MotionPlanning.h
+++ controllers/ControllerSomErSej/include/MotionPlanning.h
@@ -4,22 +4,16 @@
 class MotionPlanning
 {
 private:
+
 float a[2][5];
-
 float DataPoints[3][3];
-float theta = 0;
-float theta_d = 0;
-float theta_dd = 0;
-
-void InitiateTestData();
-
-
 
 public:
+void InitiateTestData();
 void ComputeA();
-void GetPosition(float time);
-void GetVelocity(float time);
-void GetAcceleration(float time);
+float* GetPosition(float t);
+float* GetVelocity(float t);
+float* GetAcceleration(float t);
 
 
 };
