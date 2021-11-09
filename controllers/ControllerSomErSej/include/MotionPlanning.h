c611341a984c280ea0b7f5567abcd173813fa5c4
etpe19 <etpe19@student.aau.dk>
2021-11-09 14:49:04 +0100

    Path planning

--- controllers/ControllerSomErSej/include/MotionPlanning.h
+++ controllers/ControllerSomErSej/include/MotionPlanning.h
@@ -1,19 +1,27 @@
-
-
-
 class MotionPlanning
 {
 private:
 
 float a[2][5];
-float DataPoints[3][3];
+float DP[3][3];
+float LastVel[2];
+bool debug = false;
+
+float CalculateDesiredVelocity(float x1, float y1, float x2, float y2, float x3, float y3, float t1, float t2, char coor);
 
 public:
+
+//Constructor
+MotionPlanning(){
+    LastVel[0] = 0;
+    LastVel[1] = 0;
+}
+
+
 void InitiateTestData();
 void ComputeA();
 float* GetPosition(float t);
 float* GetVelocity(float t);
 float* GetAcceleration(float t);
-
 
 };
