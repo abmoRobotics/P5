8895e7582a09607ba29dae2a2cddb612f4af20d1
abmoRobotics <56405924+abmoRobotics@users.noreply.github.com>
2021-11-16 13:02:18 +0100

    Co-authored-by: Jesper5784 <Jesper5784@users.noreply.github.com>

--- Vision/imageAligner.py
+++ Vision/imageAligner.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
-import time
+
 from cv2 import cuda
 
 
 
 
 
-def imageAligner(imgRef, imgOverlap):   
+def imageAlignerGPU(imgRef, imgOverlap):   
     MAX_FEATURES = 1000
     orb = cuda.ORB_create(MAX_FEATURES)
 
@@ -65,3 +65,51 @@ def imageAligner(imgRef, imgOverlap):
     else:
         print("Insufficient matches")
         return -1
+
+def imageAlignerCPU(img1, img2):
+
+
+    #sift = cv2.xfeatures2d.sift_create()
+    MAX_FEATURES = 100
+    sift = cv2.ORB_create(MAX_FEATURES)
+
+    # find the key points and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    match = cv2.BFMatcher()
+    matches = match.knnMatch(des1,des2,k=2)
+
+    good = []
+    for m,n in matches:
+        if m.distance < 0.03*n.distance:
+            good.append(m)
+
+    MIN_MATCH_COUNT = 10
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+        h,w = img1.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts, M)
+        traveled = abs(np.int32(dst[0][0][1]))
+        overlapHeight = h - traveled
+        return traveled, overlapHeight
+    else:
+        print("Not enought matches are found - %d/%d", (len(good)/MIN_MATCH_COUNT))
+        return 0,0
+
+if __name__ == "__main__":
+    import time
+    im1 = cv2.imread("test_data/test111.png")
+    im1 = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+    im2 = cv2.imread("test_data/test222.png")
+    im1 = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+    t1 = time.time()
+    for i in range (0,100):
+
+        imageAlignerCPU(im1,im2)
+    print((time.time()-t1)/100)
--- Vision/test_path_planning.py
+++ Vision/test_path_planning.py
@@ -12,7 +12,7 @@ img = Image.open(r"C:\P5\P5\Vision\data\train_masks\20160328_151013_361_1281.png
 img = np.array(img)
 (thresh, blackAndWhiteImage) = cv2.threshold(img, 127, 1, cv2.THRESH_BINARY)
 binary_image = closing(blackAndWhiteImage)
-
+t1 = time.time()
 sorted_cracks = process_image(binary_image)
 
 frame1 = Frame()
@@ -20,7 +20,7 @@ frame1 = Frame()
 for crack in sorted_cracks:
     frame1.add_crack(Crack(crack))
 
-t1 = time.time()
+
 find_path(frame1) # 5 ms
 print(time.time()-t1)
 frame0Vis = visualize(frame1, frame1.path,640*0.75)
